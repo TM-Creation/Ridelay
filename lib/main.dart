4e03064dd73100c6cf6a4fe9af867fc3d47c3f9b
Moeen <moeen.abdulmajeed786@gmail.com>
2024-06-26 22:23:40 +0500

    main file update

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,18 @@ import 'package:ridely/src/app.dart';
 import 'package:ridely/src/models/globals.dart' as globals;
 import 'package:ridely/src/presentation/ui/config/debug_helper.dart';
 import 'package:device_preview/device_preview.dart';
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+//   globals.cameras = await availableCameras();
+//   runApp(DevicePreview(
+//     enabled: true,
+//     builder: (context) => App(), // Wrap your app
+//   ),);
+// }
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   globals.cameras = await availableCameras();
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) => App(), // Wrap your app
-  ),);
-}
-/*Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  globals.cameras = await availableCameras();
   runApp(const App());
-}*/
+}
